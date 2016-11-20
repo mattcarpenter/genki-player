71c1f15046c4ca2a6516ede47bafd4391d8a0d7d
matthew carpenter <matt@matthews-MacBook-Pro.local>
2016-11-19 23:13:25 -0800

    checking in

--- scripts/transcribe.js
+++ scripts/transcribe.js
@@ -8,20 +8,26 @@ var fs = require('fs');
 var auth = require('./auth.json');
 var authStr = 'Basic ' + new Buffer(auth.username + ':' +auth.password).toString('base64');
 
+output = [];
+
 items.forEach(function (item) {
   var root = '/Users/matt/Desktop/genki/';
   var cmd = 'curl -X POST -H "Authorization: ' + authStr + '" -H "Content-Type: audio/flac" -H "Cache-Control: no-cache" -H "Postman-Token: a58b007a-ca28-82b5-b314-30a73f65adb9" "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?timestamps=true&word_alternatives_threshold=0.9&continuous=true&model=ja-JP_BroadbandModel" --data-binary @' + root + item.filename.replace('mp3', 'flac') + ' > ' + item.filename.replace('mp3', 'json');
   
   console.log(cmd);
 
-  exec(cmd,
+  /*exec(cmd,
   function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
     if (error !== null) {
       console.log('exec error: ' + error);
     }
-  });
+    });
+  */
+
+  output.push(cmd);
+
 });
 
-
+fs.writeFileSync('watson.sh', output.join('\n'));
